8e469251350b465b5cff19a32d3003862f95b893
asperansa <asperansa@gmail.com>
2026-06-05 18:43:09 +0500

    feat: fix header

--- src/js/static-js.js
+++ src/js/static-js.js
@@ -1,6 +1,11 @@
 import $ from "jquery";
 
 $(function() {
+  // Header: затемнение при скролле
+  $(window).on('scroll', function() {
+    $('.js-header').toggleClass('is-scrolled', $(this).scrollTop() > 50);
+  });
+
   $(".js-btn-modal").on("click", function (){
     const modal = $(this).data("modal");
     $("body").addClass("lock just-modal--default")
